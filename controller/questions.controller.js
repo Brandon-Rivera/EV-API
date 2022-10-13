25a392af6eeb031d60627269434d8e33aecf2f16
SnipperPlayer <galeana2002@hotmail.com>
2022-10-13 10:49:11 -0500

    Cosas de questions

--- controller/questions.controller.js
+++ controller/questions.controller.js
@@ -28,7 +28,7 @@ module.exports.getQuestion = (req,res) =>
 module.exports.insertQuestion = (req, res) => 
 {
     const body = req.body; 
-    const sql = `INSERT INTO questions(questionType,question,questionDescription,isActive)VALUES(?, ?, ?, ?)`;
+    const sql = `INSERT INTO questions(id,questionType,question,questionDescription,isActive, qOptions) VALUES(?, ?, ?, ?, ?, ?)`;
     conexion.query(sql, [body.questionType, body.question, body.questionDescription, body.isActive], (error, results, fields) =>{
         if(error){
             res.json({ mensaje: "Valores inválidos" });
